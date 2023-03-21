536994e57eacc12dc77ed8b29b689ab04e906214
Vorteo <92757545+Vorteo@users.noreply.github.com>
2023-03-21 10:23:01 +0100

    Add files via upload

---
+++ Threads, Async/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace cv3v2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // await Test();
+            await Experiment();
+        }
+
+
+        private static async Task Experiment()
+        {
+            Console.WriteLine("Start");
+            await Task.Delay(1000);
+            StreamWriter sw = new StreamWriter("test.txt");
+
+            await sw.WriteLineAsync("adavlvmafmaofmoamfoamf");
+            
+            await Task.Delay(1000);
+            Console.WriteLine("End");
+        }
+
+        private static async Task Test()
+        {
+            await File.WriteAllTextAsync("test.txt", "ABADAAGCAFT");
+
+            string x = await File.ReadAllTextAsync("test.txt");
+        }
+    }
+}
